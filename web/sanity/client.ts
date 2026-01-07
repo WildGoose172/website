8c5d0bbe6b22e28082e9b05d817581103b657915
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-01-07 20:29:52 +0100

    feat: sanity setup

---
+++ web/sanity/client.ts
@@ -0,0 +1,8 @@
+import { createClient } from 'next-sanity'
+
+export const client = createClient({
+  projectId: 'uwf1iyke',
+  dataset: 'production',
+  apiVersion: '2024-01-01',
+  useCdn: false,
+})
