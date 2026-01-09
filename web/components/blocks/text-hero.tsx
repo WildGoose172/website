7a7e0d9f8ef7bc602c6403a5e1c707eb99fda261
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-01-09 16:30:22 +0100

    feat: created text hero component

---
+++ web/components/blocks/text-hero.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import Image from 'next/image'
+
+import { cn } from '@/lib/utils'
+import type { TextHero } from '@/types/sanity'
+
+interface TextHeroProps extends TextHero {
+  documentId: string
+  documentType: string
+  className?: string
+}
+
+export function TextHero({ title, description, className }: TextHeroProps) {
+  return (
+    <section
+      className={cn('bg-primary flex min-h-dvh flex-col text-white', className)}
+    >
+      <div className="container mx-auto flex grow flex-col justify-around gap-20 pb-10 pt-40">
+        <Image
+          src="/images/flat-goose.webp"
+          alt="Wild Goose"
+          width={200}
+          height={146}
+          className="sm:ml-20"
+        />
+        <div className="self-center sm:self-end">
+          <h1 className="text-balance text-center text-4xl leading-[1.1] tracking-tight md:text-6xl lg:text-7xl">
+            {title}
+          </h1>
+          <p className="text-muted mx-auto max-w-prose text-balance text-center">
+            {description}
+          </p>
+        </div>
+        <div className="head-4 text-muted flex flex-col">
+          <span>Fly</span>
+          <span>Further</span>
+          <span>Together</span>
+        </div>
+      </div>
+    </section>
+  )
+}
