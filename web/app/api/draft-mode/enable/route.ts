dab8797ef22977a215d0b1fa97e67fe7987d3391
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-01-14 15:18:46 +0100

    fix: allow iframe of website

--- web/app/api/draft-mode/enable/route.ts
+++ web/app/api/draft-mode/enable/route.ts
@@ -1,6 +1,8 @@
 import { client } from '@/sanity/client'
 import { defineEnableDraftMode } from 'next-sanity/draft-mode'
 
+console.log({ token: process.env.SANITY_VIEWER_TOKEN })
+
 export const { GET } = defineEnableDraftMode({
   client: client.withConfig({
     token: process.env.SANITY_VIEWER_TOKEN,
