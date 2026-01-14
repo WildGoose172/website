524b74357c3c67aea83366001b1d685ecb91956f
Jeremy Mees <JeremyMees@users.noreply.github.com>
2026-01-14 14:29:51 +0100

    fix: allow iframe of website

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,20 @@ import { NextConfig } from 'next'
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "frame-ancestors 'self' https://*.sanity.io https://www.sanity.io",
+          },
+        ],
+      },
+    ]
+  },
   images: {
     qualities: [75, 90, 100],
     remotePatterns: [
